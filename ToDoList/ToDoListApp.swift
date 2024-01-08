a160ce9c25400facdf07daa8dce51cc3d50a99cb
udayg24 <udaygajera116@gmail.com>
2024-01-08 13:42:57 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Uday Gajera on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
